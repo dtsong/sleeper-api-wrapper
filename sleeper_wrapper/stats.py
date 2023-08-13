e02b07b4175f700eb1cc0e6ef0bc93f9f2829d10
Ford Higgins <wfordh@users.noreply.github.com>
2023-08-12 22:21:26 -0700

    remove unnecessary comment (again)

--- sleeper_wrapper/stats.py
+++ sleeper_wrapper/stats.py
@@ -21,8 +21,6 @@ class Stats(BaseApi):
 		return self._call("{}/{}/{}/{}".format(self._projections_base_url, season_type, season, week))
 
 	def get_player_week_stats(self, stats, player_id: str) -> Union[dict, None]:
-		# @wfordh 3/6/23 note: why is stats passed to it's own method?
-		# @wfordh 8/11/23 note: is the 'week' in the method name relevant here?
 		try:
 			return stats[player_id]
 		except:
